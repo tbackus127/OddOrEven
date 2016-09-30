6c0fda170e705efb08f89e9fcdfdb7394c93c46f
Tim Backus <tbackus127@gmail.com>
2016-09-30 18:51:33 -0400

    Initial and only commit (unless I think of something sillier).
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

---
+++ src/com/rath/ooe/OddOrEvenGenerator.java
@@ -0,0 +1,44 @@
+
+package com.rath.ooe;
+
+/**
+ * This class generates bad JavaScript with chains of boolean ORs to determine
+ * whether a given number is odd or even.
+ * 
+ * @author Tim Backus tbackus127@gmail.com
+ * 
+ */
+public class OddOrEvenGenerator {
+
+  /**
+   * Builds the script as a String.
+   * @param min the minimum value we have to check.
+   * @param max the maximum value we have to check.
+   * @param num the number we're checking.
+   * @return the entire JavaScript script as a String.
+   */
+  public static String generate(int min, int max, int num) {
+    String result = "";
+    result += "function isEven(num) {\n";
+    result += "  if(";
+
+    // Check if min/max are even by "rounding" towards 0.
+    min = (min % 2 == 1) ? (min < 0) ? min + 1 : min - 1 : min;
+    max = (max % 2 == 1) ? (max > 0) ? max - 1 : max + 1 : max;
+
+    // The massive chain of ORs.
+    result += "num == " + min;
+    for (int i = min + 2; i <= max; i += 2) {
+      result += " || num == " + i;
+    }
+
+    result += ") {\n";
+    result += "    return true;\n";
+    result += "  } else {\n";
+    result += "    return false;\n";
+    result += "  }\n}\n";
+    result += "ieres = isEven(ienum);";
+
+    return result;
+  }
+}
---
+++ src/com/rath/ooe/TestOoE.java
@@ -0,0 +1,33 @@
+
+package com.rath.ooe;
+
+import java.util.Scanner;
+
+public class TestOoE {
+
+  public static void main(String[] args) {
+    Scanner cin = new Scanner(System.in);
+    OddOrEven ooe = new OddOrEven();
+    boolean running = true;
+    
+    while(running) {
+      System.out.print("Enter a number: ");
+      int num = 0;
+      try {
+        num = cin.nextInt();
+      }
+      catch (NumberFormatException nfe) {
+        System.out.println("Please enter a valid 32-bit number.");
+        running = false;
+      }
+      
+      if (ooe.isEven(num)) {
+        System.out.println("Your number is even!");
+      } else {
+        System.out.println("Your number is odd!");
+      }
+    }
+    
+    cin.close();
+  }
+}
